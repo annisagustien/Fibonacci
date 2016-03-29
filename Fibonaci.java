497b8fa2eb710919510da1e07706cada4625bb9f
annisagustien <annisagw@gmail.com>
2016-03-29 15:56:56 +0700

    Added files via upload

--- Fibonaci.java
+++ Fibonaci.java
@@ -1,29 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package jurnal1;
 
 import java.util.ArrayList;
 
-
 public class Fibonaci {
+    ArrayList<Integer> al = new ArrayList<>();
     
-    private ArrayList<Integer> al = new ArrayList<>();
-    
-    public Fibonaci() {
-    }
-    
-    public void performFibonaci(int i){
-        int prevInt = 0;
-        int nextInt = 1;
+    public void performReverseOfFibonaci(int i){
         for (int j = 0; j < i; j++) {
             if ( j == 0 || j == 1){
-                System.out.println("1");
                 al.add(1);
                 continue;
             }
             al.add(al.get(j-1)+al.get(j-2));
-            System.out.println("" + al.get(j));
         }
         
+        for (int j = i-1; j >= 0; j--) {
+            System.out.println("" + al.get(j));
+        }
     }
-    
-    
 }
