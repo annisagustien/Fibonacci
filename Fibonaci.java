dddbf9f23eab37e4207b9a3d6c6fdd6a83a40c47
annisagustien <annisagw@gmail.com>
2016-03-29 15:55:42 +0700

    Added files via upload

--- Fibonaci.java
+++ Fibonaci.java
@@ -1,29 +1,24 @@
+
 package jurnal1;
 
 import java.util.ArrayList;
 
-
 public class Fibonaci {
+    ArrayList<Integer> al = new ArrayList<>();
     
-    private ArrayList<Integer> al = new ArrayList<>();
-    
-    public Fibonaci() {
-    }
-    
-    public void performFibonaci(int i){
-        int prevInt = 0;
-        int nextInt = 1;
+    public void performSumOfFibonaci(int i){
+        int sum = 0;
         for (int j = 0; j < i; j++) {
             if ( j == 0 || j == 1){
-                System.out.println("1");
                 al.add(1);
+                sum += 1;
                 continue;
             }
             al.add(al.get(j-1)+al.get(j-2));
-            System.out.println("" + al.get(j));
+            sum += al.get(j);
         }
         
+        System.out.println("" + sum);
+        
     }
-    
-    
 }
